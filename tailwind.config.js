1b0d78105bc50a5a22f7790da5c9feb225ca70d9
LeeeeHaeji <gotqks013@gmail.com>
2024-03-09 03:58:59 +0900

    회원가입, 메인 UI 작업

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,23 @@ export default {
       backgroundSize: {
         "50%": "50%",
       },
+      backgroundImage: {
+        "custom-gradient": "linear-gradient(to top, #ADBC9F 50%, transparent 50%)",
+        "custom-gradient-white": "linear-gradient(to top, #fff 50%, transparent 50%)",
+      },
       boxShadow: {
         inner: "inset 0 0 0 2px #faba10",
+        white: "0 4px 6px -1px rgba(255, 255, 255, 0.1), 0 -2px 6px 1px rgba(255, 255, 255, 0.06)",
+      },
+      screens: {
+        tablet: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        laptop: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        desktop: "1280px",
+        // => @media (min-width: 1280px) { ... }
       },
     },
   },
